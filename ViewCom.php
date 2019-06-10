a6e1f61a5f3c2d2aba8860a13b7443578bc21820
Gabriel <gabriel.rodrigues319@etec.sp.gov.br>
2019-06-10 00:18:33 -0300

    Melhoras
    Deixar ele mais bonito com os botões e tematizar ele com o tema do resto do site.

--- ViewCom.php
+++ ViewCom.php
@@ -6,8 +6,16 @@
     <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 
-<body>
-    <a href="RegCom.html">Deixar um comentario</a><br><br>
+<body bgcolor="black" style="color: green;">
+    <table>
+        <tr>
+            <td><a href="javascript:history.back();"><img src="image/voltar.png" onmouseout="this.src='image/voltar.png'" onmouseover="this.src='image/voltar_hover.png'" width="100px" /></a></td>
+            <td><a href="RegCom.html">Deixar um comentario</a></td>
+        </tr>
+        <tr>
+            <td><p>Sessão de Comentarios</p></td>
+        </tr>
+    </table>
     <?php
     $con = mysqli_connect('localhost', 'root', '', 'comen');
     if ($con->connect_error) {
@@ -20,17 +28,17 @@
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             ?>
-            <table width="30%">
+            <table border="1">
                 <tr>
-                    <td style="font-size: 13px; color: blueviolet"><?php echo $row["name"] ?></td>
+                    <td style="font-size: 13px; color: blueviolet;"><?php echo $row["name"] ?></td>
                 </tr>
                 <tr>
-                    <td><?php echo $row["comentario"] ?></td>
+                    <td style="color: green;"><?php echo $row["comentario"] ?></td>
                 </tr>
                 <tr>
-                    <td style="font-size: 10px"><?php echo $row["DH"] ?></td>
+                    <td style="color: green; font-size: 10px"><?php echo $row["DH"] ?></td>
                 </tr>
-                    <br><br>
+                <br />
             </table>
         <?php
     }
